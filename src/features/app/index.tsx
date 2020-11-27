c19c1e4dffeb70c374e5c4b8d0c03cc8e9e8f334
Benjamin Olson <ben@whynotben.com>
2020-11-27 16:56:26 -0500

    Add button grid for theme building

--- src/features/app/index.tsx
+++ src/features/app/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { EuiPage } from '@elastic/eui'
 import SignIn from 'features/sign-in'
+import DemonstrativeStuff from 'features/demonstrative-stuff'
 
 const App = () => (
   <EuiPage>
     <SignIn />
+    <DemonstrativeStuff />
   </EuiPage>
 )
 
