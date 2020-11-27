b5ab9b6578b3706c54ec1b33213eb074dd458523
Benjamin Olson <ben@whynotben.com>
2020-11-27 10:59:21 -0500

    Remove webkit tap highlight

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import './index.scss'
+import './index.scss'
 import App from 'features/app'
 import FirebaseProvider from 'features/firebase'
 // import reportWebVitals from './reportWebVitals'
